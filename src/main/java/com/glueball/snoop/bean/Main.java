e905bd853ebbcaeee2186782447ce0be35f42ccc
Fuzeseri Karoly <karoly@fuzeseri.hu>
2015-01-02 18:27:35 +0100

    jetty config bugfix, default properties, maven gwt plugin

--- src/main/java/com/glueball/snoop/bean/Main.java
+++ src/main/java/com/glueball/snoop/bean/Main.java
@@ -1,35 +1,55 @@
 package com.glueball.snoop.bean;
 
-import java.io.IOException;
-
 import org.apache.cxf.Bus;
 import org.apache.cxf.bus.spring.SpringBusFactory;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.spring.JAXRSServerFactoryBeanDefinitionParser.SpringJAXRSServerFactoryBean;
-import org.apache.tika.exception.TikaException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.xml.sax.SAXException;
+import org.springframework.core.io.ClassPathResource;
 
 public class Main {
 
 	private static final SpringBusFactory busFactory = new SpringBusFactory();
-	private static final String JETTY_CONTEXT_XML = "src/main/resources/spring/jetty-server.xml";
 
-	public static void main(final String[] args) throws InterruptedException, IOException, SAXException, TikaException {
+	private final ApplicationContext ctx;
+	private final Bus bus;
+	private final JAXRSServerFactoryBean jaxRsServerFactory;
+	private Server server;
 
-		final Bus bus = busFactory.createBus(JETTY_CONTEXT_XML);
-		//SpringBusFactory.setDefaultBus(bus);
+	public Main() {
 
-		@SuppressWarnings("resource")
-		final ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring/application.xml");
+//		final String JETTY_CONTEXT_XML = "file:src"
+//	            + File.separator + "main" 
+//	            + File.separator + "resources" 
+//	            + File.separator + "spring" 
+//	            + File.separator + "jetty-server.xml";
+
+		ctx = new ClassPathXmlApplicationContext("classpath:spring/application.xml");
 		final String address = (String) ctx.getBean("listenAddress");
-		final JAXRSServerFactoryBean jaxRsServerFactory = (SpringJAXRSServerFactoryBean) ctx.getBean("restContainer");
+
+		bus = busFactory.createBus(new ClassPathResource("spring/jetty-server.xml").getPath());
+		
+		jaxRsServerFactory = (SpringJAXRSServerFactoryBean) ctx.getBean("restContainer");
 		jaxRsServerFactory.setBus(bus);
 		jaxRsServerFactory.setAddress(address);
-		@SuppressWarnings("unused")
-		final Server server = jaxRsServerFactory.create();
+	}
 
+	public static void main(final String[] args) {
+
+		final Main main = new Main();
+		main.start();
+	}
+
+	public void start() {
+
+		this.server = jaxRsServerFactory.create();
+	}
+
+	public void stop() {
+
+		this.server.destroy();
+		System.exit(1);
 	}
 }
